d48fa7b69e8f1001f9e9a912b35cae2723919132
ozo <72322089+o0omega@users.noreply.github.com>
2025-10-24 21:27:59 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-REM Build Tracker.py into .exe, it will appear in new Builds folder
+REM Build Tracker.py into .exe, it will appear in Builds folder
 
 pyinstaller --onefile --distpath "Builds" --clean Tracker.py
 rmdir /s /q build
